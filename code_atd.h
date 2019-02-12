2a1e216b84c155b5ada0f26385de7ec020ef7dac
Macqusha <md9793@ya.ru>
2019-02-12 21:16:07 +0700

    v2.1.0 new class Numeric added

--- code_atd.h
+++ code_atd.h
@@ -2,19 +2,21 @@
 #define __code_atd__
 // Подключение необходимых типов данных
 #include "zamena_atd.h"		
-#include "cezar_atd.h"		
+#include "cezar_atd.h"	
+#include "numeric_atd.h"
 namespace simple_codes 
 {
 	// структура, обобщающая все имеющиеся шифры
 	struct code 
 	{
 		// значения ключей для каждого из шифров
-		enum key {ZAMENA, CEZAR};
+		enum key {ZAMENA, CEZAR, NUMERIC};
 		key k; // ключ
 		// используемые альтернативы
 		union { // используем включение
 			zamena r;			
-			cezar t;			
+			cezar t;
+			numeric b;
 		};
 		char message[20]; //исходный текст
 		//Warning. Меняя размер массива, изменить также условие в коде зашифрования в файлах cezars.cpp и zamenas.cpp. 
---
+++ numeric_atd.h
@@ -0,0 +1,11 @@
+#ifndef __numeric_atd__
+#define __numeric_atd__
+
+namespace simple_codes {
+	// רטפנ צטפנמגמי חאלוםû
+	struct numeric {
+		int digit; // ןאנאלוענ רטפנא
+	};
+} // end simple_codes namespace
+#endif
+#pragma once
