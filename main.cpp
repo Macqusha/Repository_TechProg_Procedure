62f3a730bfbc7c3dcb99089b323968cfdd18c702
Macqusha <md9793@ya.ru>
2019-04-01 00:20:45 +0700

    hotfix secure
    - Исправлена ошибка !ifst.eof()
    - Убрано много лишего кода
    - Удалена ненужная переменная error_code

--- main.cpp
+++ main.cpp
@@ -22,24 +22,24 @@ int main(int argc, char* argv[])
 {
 	if (argc != 3)
 	{
-		cout << "incorrect command line! "
+		cout << "Incorrect command line! "
 			"Waited: command infile outfile" << endl;
-		exit(1);
+		return -8;
 	}
 	ifstream ifst(argv[1]);
 	ofstream ofst(argv[2]);
-	// Начало проверок
+
 	// Проверка наличия файла входных данных
 	if (!ifst.is_open())
 	{
 		cout << "Error opening in.txt";
-		return -5;
+		return -6;
 	}
 	// Проверка возможности создания файла выходных данных
 	if (!ofst.is_open())
 	{
 		cout << "Error opening out.txt";
-		return -6;
+		return -7;
 	}
 
 	// Проверка содержимого входного файла
@@ -49,70 +49,58 @@ int main(int argc, char* argv[])
 	int param; // Параметр шифра, с которым работаем
 	string mes; // Исходный текст шифра, с которым работаем
 	string owner; // Владелец шифра, с которым работаем
-	int error_code = 0; // Код ошибки
 
 	// Цикл проверки
-	while (!ofst.eof() && (error_code == 0))
+	while (!ifst.eof())
 	{
 		switch (i % 4) {
 		case 0:
-			if (!(ifst >> typecode)) // Нельзя считать число
-			{
-				if(!(ifst >> buferStr) && (i > 0) && (typecode != 0)) // Конец данных и не пустой файл - ошибки нет
-					goto noerrors;
-				else  // Пустой файл или ошибка в данных
-				{
-					error_code = 1;
-				}
-			}
-			else
-			{
-				if ((typecode < 1) || (typecode > 3)) // Ошибка в данных
-				{
-					error_code = 1;
-				}
-			}
 			i++;
+			if (!(ifst >> typecode) || (typecode < 1) || (typecode > 3)) // Нельзя считать число или некорректный ключ
+			{
+				cout << "Error. Wrong type. Parametr #" << i << "." << endl;
+				return -1;
+			}
 			break;
 		case 1:
-			if (!(ifst >> param)) error_code = 2;	// Внезапный конец файла или не число
-			if ((typecode == 1) && ((param < 1) || (param > 3))) error_code = 2; // Несоответствие параметра допустимым ключам 1, 2, 3
-			if ((typecode == 2) && (param < 0)) error_code = 2; // Несоответствие параметра допустимым сдвигам (неотрицательное)
-			if ((typecode == 3) && (param < 0)) error_code = 2; // Несоответствие параметра допустимым числам (неотрицательное)
 			i++;
+			if (!(ifst >> param) ) // Нельзя считать число
+			{
+				cout << "Error. Wrong parameter. Parametr #" << i << "." << endl;
+				return -2;
+			}
+			if ((param < 0) || ((typecode == 1) && ((param < 1) || (param > 3))))  // Некорректный параметр
+			{
+				cout << "Error. Wrong parameter. Parametr #" << i << "." << endl;
+				return -2;
+			} 
 			break;
 		case 2:
-			if (!(ifst >> mes)) error_code = 3; // Внезапный конец файла 
 			i++;
+			if (!(ifst >> mes)) // Нельзя считать строку
+			{
+				cout << "Error. Wrong message. Parametr #" << i << "." << endl;
+				return -3;
+			}
 			break;
 		case 3:
-			if (!(ifst >> owner)) error_code = 4; // Внезапный конец файла 
 			i++;
+			if (!(ifst >> owner)) // Нельзя считать строку
+			{
+				cout << "Error. Wrong owner. Parametr #" << i << "." << endl;
+				return -4;
+			}
 			break;
 		}
 	}
-	// Определение типа ошибки
-	switch (error_code) {
-	case 1:
-		cout << "Error. Wrong type. Parametr #" << i << "."<< endl;
-		return -1;
-		break;
-	case 2:
-		cout << "Error. Wrong parameter. Parametr #" << i << "." << endl;
-		return -2;
-		break;
-	case 3:
-		cout << "Error. Wrong message. Parametr #" << i << "." << endl;
-		return -3;
-		break;
-	case 4:
-		cout << "Error. Wrong owner. Parametr #" << i << "." << endl;
-		return -4;
-		break;
+
+	// Проверка, закончился ли файл в процессе введения шифра
+	if (i % 4 != 0)
+	{
+		cout << "Error. Unexpected end of file. Parametr #" << i << "." << endl;
+		return -5;
 	}
 
-	// Проверка оконечна без ошибок
-noerrors: ;
 	// Переоткрытие файла входных данных
 	ifst.close();
 	ifst.open(argv[1]);
