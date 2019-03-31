5405fa8d9af2c2604af3415a3ba588bb87ecf289
Macqusha <md9793@ya.ru>
2019-03-31 19:00:34 +0700

    Main rewrited secure

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 ﻿// Procedure.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 #include <iostream>
 #include <fstream>
+#include <string>
 #include "conteiner.h"
 
 using namespace std;
@@ -27,20 +28,88 @@ int main(int argc, char* argv[])
 		exit(1);
 	}
 	ifstream ifst(argv[1]);
-	//проверка, что в файле только цифры  - не актуальна
-	/*char symb;
-	while (!ifst.eof())
+	ofstream ofst(argv[2]);
+	//Начало проверок
+	//Проверка наличия файла входных данных
+	if (!ifst.is_open())
 	{
-		ifst >> symb;
-		if ((symb < 48 || symb >57) && symb != 45 && symb != 46)
-		{
-			cout << "Incorrect input file!" << endl;
-			exit(1);
+		cout << "Error opening in.txt";
+		return -5;
+	}
+	//Проверка возможности создания файла выходных данных
+	if (!ofst.is_open())
+	{
+		cout << "Error opening out.txt";
+		return -6;
+	}
+
+	//Проверка содержимого входного файла
+	int i = 0; //Номер строки, которую будем считывать
+	char* buferStr = new char; //Буфер для строки
+	int typecode; //Тип шифра, с которым работаем
+	int param; //Параметр шифра, с которым работаем
+	string mes; //Исходный текст шифра, с которым работаем
+	string owner; //Владелец шифра, с которым работаем
+	int error_code = 0; //Код ошибки
+	//Цикл проверки
+	while (!ofst.eof() && (error_code == 0))
+	{
+		switch (i % 4) {
+		case 0:
+			if (!(ifst >> typecode)) //Нельзя считать число
+			{
+				if(!(ifst >> buferStr) && (i > 0)) //Конец данных и не пустой файл - ошибки нет.
+					goto noerrors;
+				else  //Пустой файл или ошибка в данных
+				{
+					error_code = 1;
+					break;
+				}
+			}
+			i++;
+			break;
+		case 1:
+			if (!(ifst >> param)) error_code = 2;	//Внезапный конец файла или не число
+			if ((typecode == 1) && ((param < 1) || (param > 3))) error_code = 2; //Несоответствие параметра допустимым ключам 1, 2, 3
+			if ((typecode == 2) && (param < 0)) error_code = 2; //Несоответствие параметра допустимым сдвигам 0, 1, ...
+			if ((typecode == 3) && (param < 0)) error_code = 2; //Несоответствие параметра допустимым числам 0, 1, ...
+			i++;
+			break;
+		case 2:
+			if (!(ifst >> mes)) error_code = 3; //Внезапный конец файла 
+			i++;
+			break;
+		case 3:
+			if (!(ifst >> owner)) error_code = 4; //Внезапный конец файла 
+			i++;
+			break;
 		}
 	}
+	//Определение ошибки
+	switch (error_code) {
+	case 1:
+		cout << "Error. Wrong type." << endl;
+		return -1;
+		break;
+	case 2:
+		cout << "Error. Wrong parameter." << endl;
+		return -2;
+		break;
+	case 3:
+		cout << "Error. Wrong message." << endl;
+		return -3;
+		break;
+	case 4:
+		cout << "Error. Wrong owner." << endl;
+		return -4;
+		break;
+	}
+
+	//Проверка оконечна без ошибок
+noerrors: ;
+	//Переоткрытие файла входных данных
 	ifst.close();
-	ifst.open(argv[1]);			*/
-	ofstream ofst(argv[2]);
+	ifst.open(argv[1]);
 
 	cout << "Start" << endl;
 	container c;
@@ -57,6 +126,7 @@ int main(int argc, char* argv[])
 	Out(c, ofst);
 	cout << "Stop" << endl;
 
-	system("pause");
+	ifst.close();
+	ofst.close();
 	return 0;
 }
